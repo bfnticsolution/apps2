b11c1430f68163285403a2be86fa892ea0099d96
NTIC Solution <nticsolution.bf@gmail.com>
2025-06-15 13:32:09 +0000

    config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Configuration de la base de données
-define('DB_HOST', 'localhost'); // L'hôte de la base de données
+define('DB_HOST', 'github.io'); // L'hôte de la base de données
 define('DB_NAME', 'premiumshop'); // Le nom de votre base de données
 define('DB_USER', 'root'); // Le nom d'utilisateur pour accéder à la base de données
 define('DB_PASS', ''); // Le mot de passe pour accéder à la base de données
@@ -10,5 +10,5 @@ define('APP_ENV', 'development'); // Environnement de l'application (development
 define('DEBUG_MODE', true); // Mode de débogage
 
 // Optionnel : Configuration des chemins de l'application
-define('BASE_URL', 'http://localhost/'); // URL de base de l'application
+define('BASE_URL', 'https://bfnticsolution.github.io/'); // URL de base de l'application
 ?>
